74c68d41adb0a64760b2915ba2660c23da5b42a8
Hugh Keenan <hugh.keenan114@gmail.com>
2024-06-13 19:54:43 +0000

    define function to present choices in first room

--- run.py
+++ run.py
@@ -2,18 +2,26 @@
 def intro():
     print("You are standing outside of the house you plan to rob. Do you...")
     
-    print("A: Open the door\n B:Kick the door")
+    print("A: Open the door")
+    print("B: Kick the door")
 
-    str(input())  
+    answer = str(input())
+    if answer == "a" or "A":
+        print("Correct")
+        first_room()
+    elif answer == "b" or "B":
+        print("wrong")   
+
+def first_room():
+    print("You are in the first room")
 
 def main():
     print("Welcome to Word Burglar. In this text-based game you will make choices to pull off a heist.\n Your objective is to find the priceless painting hidden somewhere in the house.")
-    
     print("As you progress through the game, you will be presented with choices A-D as you enter each room.\n Make your choice by typing the letter corresponding to the one you want.")
-    
     name = str(input("To begin, please enter your name: "))
-    
     print(f"Welcome to the game {name}")
+    intro()
+
 
 
 main()
