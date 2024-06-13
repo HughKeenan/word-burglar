79ce4369da5cb98bfff991929a0b076504463450
Hugh Keenan <hugh.keenan114@gmail.com>
2024-06-13 19:24:30 +0000

    define main function to run game and function to allow player to enter name

---
+++ run.py
@@ -0,0 +1,12 @@
+
+PLAYER_NAME = enter_name(name)
+
+def enter_name():
+    name = str(input("To begin, please enter your name: "))
+
+def main():
+    print("Welcome to Word Burglar. In this text-based game you will make choices to pull off a heist.\n Your objective is to find the priceless painting hidden somewhere in the house.")
+    print("As you progress through the game, you will be presented with choices A-D as you enter each room.\n Make your choice by typing the letter corresponding to the one you want.")
+
+
+main()
