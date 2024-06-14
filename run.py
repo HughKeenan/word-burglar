19826144f356c799d61429a0af5e2cda6afc69a3
Hugh Keenan <hugh.keenan114@gmail.com>
2024-06-14 19:14:31 +0000

    define function to implement safe opening minigame

--- run.py
+++ run.py
@@ -34,7 +34,6 @@ def first_room():
 
 def last_room():
     print("You are in the last room. Do you...")
-
     print("A: Open the safe")
     print("B: Go back to the first room")
     print("C: Wake the residents up and surrender")
@@ -42,13 +41,18 @@ def last_room():
     while True:
         answer = str(input())
         if answer == "a" or answer == "A":
-            last_room()  
+            safecracker()  
         elif answer == "b" or answer == "B":
             first_room()
         elif answer == "c" or answer == "C":
             game_over()    
         else:
             print("Please choose a valid option")
+
+def safecracker():
+    print("You approach the safe. On the front is a 10 digit keypad.")
+    print(" You remember from casing the house that thecode is 3 digits long and the first one is 2.")
+    print("Be careful! You've seen this kind of safe before, if you enter more than 5 wrong numbers, it will set off the alarm and the police will be here in minutes!")
 
 def game_over():
     print("You have been arrested, game over")
@@ -63,7 +67,6 @@ def game_over():
         else:
             print("Please choose a valid option")
     
-
 def main():
     print("Welcome to Word Burglar. In this text-based game you will make choices to pull off a heist.\n Your objective is to find the priceless painting hidden somewhere in the house.")
     print("As you progress through the game, you will be presented with choices A-D as you enter each room.\n Make your choice by typing the letter corresponding to the one you want.")
